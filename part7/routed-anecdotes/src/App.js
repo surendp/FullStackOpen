cc1d55d18deb5c5098914c2da543224bb9187eb3
Surendra Pandey <surendra.pandey2@metropolia.fi>
2020-08-11 19:44:06 +0300

    - complete exercises 7.4 to 7.8

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -6,6 +6,7 @@ import {
   useRouteMatch,
   useHistory,
 } from 'react-router-dom'
+import { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -56,23 +57,26 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const [content, resetContent] = useField('text')
+  const [author, resetAuthor] = useField('text')
+  const [info, resetInfo] = useField('text')
   const history = useHistory()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
-    setContent('')
-    setAuthor('')
-    setInfo('')
     history.push('/')
+  }
+
+  const handleReset = () => {
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
@@ -81,18 +85,19 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
       </form>
+      <button onClick={handleReset}>reset</button>
     </div>
   )
 
---
+++ part7/ultimate-hooks/src/App.js
@@ -0,0 +1,104 @@
+  
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  const reset = () => {
+    setValue('')
+  }
+
+  return [{
+    type,
+    value,
+    onChange
+  }, reset]
+}
+
+const useResource = (baseUrl) => {
+  const [resources, setResources] = useState([])
+
+  // fetch all resources from the database
+  // during the first render of the component
+  useEffect(() => {
+    getAll()
+  }, [])
+
+  // get all the resources from the database
+  const getAll = () => {
+    axios.get(baseUrl)
+      .then(response => {
+        setResources(response.data)
+      })
+  }
+
+  // create new resource in the database
+  const create = (resource) => {
+    axios.post(baseUrl, resource)
+      .then(response => {
+        setResources([
+          ...resources,
+          response.data
+        ])
+      })
+  }
+
+  const service = {
+    create,
+    getAll,
+  }
+
+  return [
+    resources,
+    service
+  ]
+}
+
+const App = () => {
+  const [content, resetContent] = useField('text')
+  const [name, resetName] = useField('text')
+  const [number, resetNumber] = useField('text')
+
+  const [notes, noteService] = useResource('http://localhost:3005/notes')
+  const [persons, personService] = useResource('http://localhost:3005/persons')
+
+  const handleNoteSubmit = (event) => {
+    event.preventDefault()
+    noteService.create({ content: content.value })
+    resetContent('')
+  }
+ 
+  const handlePersonSubmit = (event) => {
+    event.preventDefault()
+    personService.create({ name: name.value, number: number.value})
+    resetName()
+    resetNumber()
+  }
+
+  return (
+    <div>
+      <h2>notes</h2>
+      <form onSubmit={handleNoteSubmit}>
+        <input {...content} />
+        <button>create</button>
+      </form>
+      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+
+      <h2>persons</h2>
+      <form onSubmit={handlePersonSubmit}>
+        name <input {...name} /> <br/>
+        number <input {...number} />
+        <button>create</button>
+      </form>
+      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+    </div>
+  )
+}
+
+export default App
