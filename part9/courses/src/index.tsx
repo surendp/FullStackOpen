b22d9425736ac16cec9f83764e7ca7b98a0a3e01
Surendra Pandey <surendra.pandey2@metropolia.fi>
2020-10-07 12:43:56 +0300

    - Complete exercise 9.15

--- part9/courses/src/index.tsx
+++ part9/courses/src/index.tsx
@@ -5,32 +5,79 @@ interface HeaderProps {
   courseName: string;
 }
 
-interface Part {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
 }
 
+interface CourseBasePartWithDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartOne extends CourseBasePartWithDescription {
+  name: "Fundamentals";
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: "Using props to pass data";
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends CourseBasePartWithDescription {
+  name: "Deeper type usage";
+  exerciseSubmissionLink: string;
+}
+
+interface CoursePartFour extends CourseBasePartWithDescription {
+  name: "Typescript with React";
+  peerReview: number;
+}
+
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree | CoursePartFour;
+
 interface ContentProps {
-  courseParts: Array<Part>;
+  courseParts: Array<CoursePart>;
+}
+
+interface PartProps {
+  part: CoursePart;
 }
 
 const Header: FC<HeaderProps> = ({ courseName }) => (
   <h1>{courseName}</h1>
 );
 
-const Part: FC<Part> = ({ name, exerciseCount }) => (
-  <p>
-    {name} {exerciseCount}
-  </p>
-);
+const Part: FC<PartProps> = ({ part }) => {
+  /**
+   * Helper function for exhaustive type checking
+   */
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
+  };
+
+  switch (part.name) {
+    case "Fundamentals":
+      return <p>{part.name} {part.exerciseCount} {part.description}</p>;
+    case "Using props to pass data":
+      return <p>{part.name} {part.exerciseCount} {part.groupProjectCount}</p>;
+    case "Deeper type usage":
+      return <p>{part.name} {part.exerciseCount} {part.description} {part.exerciseSubmissionLink}</p>;
+    case "Typescript with React":
+      return <p>{part.name} {part.exerciseCount} {part.description} {part.peerReview}</p>;
+    default:
+      return assertNever(part);
+  }
+};
 
 const Content: FC<ContentProps> = ({ courseParts }) => (
   <>
     {
-      courseParts.map(({ name, exerciseCount }) => (
+      courseParts.map(part => (
         <Part
-          name={name}
-          exerciseCount={exerciseCount}
+          key={part.name}
+          part={part}
         />
       ))
     }
@@ -46,18 +93,28 @@ const Total: FC<ContentProps> = ({ courseParts }) => (
 
 const App: FC = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
+    },
+    {
+      name: "Typescript with React",
+      exerciseCount: 7,
+      description: "With clear instructions it is easy to learn typescript with react",
+      peerReview: 10,
     }
   ];
 
